b240566eb8c530359cdfd7e7717ed7cfe5b65a45
ChiaYi-LIN <b04704016@ntu.edu.tw>
2022-12-08 22:49:17 +0800

    add dataset to crop image to square

--- mit_semseg/dataset.py
+++ mit_semseg/dataset.py
@@ -2,6 +2,8 @@ import os
 import json
 import torch
 from torchvision import transforms
+import random
+import math
 import numpy as np
 from PIL import Image
 
@@ -18,6 +20,21 @@ def imresize(im, size, interp='bilinear'):
 
     return im.resize(size, resample)
 
+
+def randomCrop(img, mask, width, height):
+    img = np.array(img)
+    mask = np.array(mask)
+    assert img.shape[0] >= height
+    assert img.shape[1] >= width
+    assert img.shape[0] == mask.shape[0]
+    assert img.shape[1] == mask.shape[1]
+    x = random.randint(0, img.shape[1] - width)
+    y = random.randint(0, img.shape[0] - height)
+    img = img[y:y+height, x:x+width, :]
+    mask = mask[y:y+height, x:x+width]
+    img = Image.fromarray(np.uint8(img)).convert("RGB")
+    mask = Image.fromarray(np.uint8(mask)).convert("L")
+    return img, mask
 
 class BaseDataset(torch.utils.data.Dataset):
     def __init__(self, odgt, opt, **kwargs):
@@ -296,6 +313,121 @@ class TestDataset(BaseDataset):
 
     def __len__(self):
         return self.num_sample
+    
+
+class TrainDatasetSquareCrop(BaseDataset):
+    def __init__(self, root_dataset, odgt, opt, batch_per_gpu=1, **kwargs):
+        super(TrainDatasetSquareCrop, self).__init__(odgt, opt, **kwargs)
+        self.root_dataset = root_dataset
+        self.batch_per_gpu = batch_per_gpu
+
+        # classify images into two classes: 1. h > w and 2. h <= w
+        self.batch_record_list = [[], []]
+
+        # override dataset length when trainig with batch_per_gpu > 1
+        self.cur_idx = 0
+        self.if_shuffled = False
+
+    def _get_sub_batch(self):
+        while True:
+            # get a sample record
+            this_sample = self.list_sample[self.cur_idx]
+            if this_sample['height'] > this_sample['width']:
+                self.batch_record_list[0].append(this_sample) # h > w, go to 1st class
+            else:
+                self.batch_record_list[1].append(this_sample) # h <= w, go to 2nd class
+
+            # update current sample pointer
+            self.cur_idx += 1
+            if self.cur_idx >= self.num_sample:
+                self.cur_idx = 0
+                np.random.shuffle(self.list_sample)
+
+            if len(self.batch_record_list[0]) == self.batch_per_gpu:
+                batch_records = self.batch_record_list[0]
+                self.batch_record_list[0] = []
+                break
+            elif len(self.batch_record_list[1]) == self.batch_per_gpu:
+                batch_records = self.batch_record_list[1]
+                self.batch_record_list[1] = []
+                break
+        return batch_records
+
+    def __getitem__(self, index):
+        # NOTE: random shuffle for the first time. shuffle in __init__ is useless
+        if not self.if_shuffled:
+            np.random.seed(index)
+            np.random.shuffle(self.list_sample)
+            self.if_shuffled = True
+
+        # get sub-batch candidates
+        batch_records = self._get_sub_batch()
+
+        # resize all images' short edges to the chosen size
+        if isinstance(self.imgSizes, list) or isinstance(self.imgSizes, tuple):
+            this_short_size = np.random.choice(self.imgSizes)
+        else:
+            this_short_size = self.imgSizes
+        this_short_size = math.floor(this_short_size / 64) * 64
+
+        # calculate the BATCH's height and width
+        # since we concat more than one samples, the batch's h and w shall be larger than EACH sample
+        batch_widths = np.zeros(self.batch_per_gpu, np.int32)
+        batch_heights = np.zeros(self.batch_per_gpu, np.int32)
+        for i in range(self.batch_per_gpu):
+            img_height, img_width = batch_records[i]['height'], batch_records[i]['width']
+            this_scale = this_short_size / min(img_height, img_width)
+            batch_widths[i] = math.ceil(img_width * this_scale)
+            batch_heights[i] = math.ceil(img_height * this_scale)
+
+        batch_images = torch.zeros(
+            self.batch_per_gpu, 3, this_short_size, this_short_size)
+        batch_segms = torch.zeros(
+            self.batch_per_gpu, this_short_size, this_short_size).long()
+
+        for i in range(self.batch_per_gpu):
+            this_record = batch_records[i]
+
+            # load image and label
+            image_path = os.path.join(self.root_dataset, this_record['fpath_img'])
+            segm_path = os.path.join(self.root_dataset, this_record['fpath_segm'])
+
+            img = Image.open(image_path).convert('RGB')
+            segm = Image.open(segm_path)
+            assert(segm.mode == "L")
+            assert(img.size[0] == segm.size[0])
+            assert(img.size[1] == segm.size[1])
+
+            # random_flip
+            if np.random.choice([0, 1]):
+                img = img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+                segm = segm.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+
+            # note that each sample within a mini batch has different scale param
+            img = imresize(img, (batch_widths[i], batch_heights[i]), interp='bilinear')
+            segm = imresize(segm, (batch_widths[i], batch_heights[i]), interp='nearest')
+
+            img, segm = randomCrop(img, segm, this_short_size, this_short_size)
+            segm = imresize(segm, (this_short_size, this_short_size), interp='nearest')
+
+            # image transform, to torch float tensor 3xHxW
+            img = self.img_transform(img)
+
+            # segm transform, to torch long tensor HxW
+            segm = self.segm_transform(segm)
+
+            # put into batch arrays
+            batch_images[i][:, :img.shape[1], :img.shape[2]] = img
+            batch_segms[i][:segm.shape[0], :segm.shape[1]] = segm
+
+        output = dict()
+        output['img_data'] = batch_images
+        output['seg_label'] = batch_segms
+        return output
+
+    def __len__(self):
+        return int(1e10) # It's a fake length due to the trick that every loader maintains its own list
+        #return self.num_sampleclass
 
 if __name__ == "__main__":
     from config import cfg
